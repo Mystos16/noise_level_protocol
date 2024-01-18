939f8c434b035a939f1c971001ffa1fa83ff916a
Gijs van Zoelen <gijsvanzoelen@outlook.com>
2024-01-18 13:57:26 +0200

    Update py_plot_today.py

--- py_plot_today.py
+++ py_plot_today.py
@@ -15,7 +15,7 @@ from matplotlib.ticker import AutoMinorLocator
 
 #date changed to today
 tomorrow = date.today()
-filename_csv = "/home/pi/noise/csv/" + tomorrow.strftime("%Y%m%d") + ".csv"
+filename_csv = "/home/techpro2/noise/csv/" + tomorrow.strftime("%Y%m%d") + ".csv"
 i = int(tomorrow.strftime("%Y"))
 j = int(tomorrow.strftime("%m"))
 k = int(tomorrow.strftime("%d"))
@@ -86,4 +86,4 @@ plt.xlim(
 )
 fig.autofmt_xdate()
 fig.set_size_inches(14,10)
-plt.savefig('/home/pi/noise/pic/' + pic_title + '.png', bbox_inches='tight')
+plt.savefig('/home/techpro2/noise/pic/' + pic_title + '.png', bbox_inches='tight')
